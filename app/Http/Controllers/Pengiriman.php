6c347d5220bcd494df55dab93ee235bb644dc94a
kyrozyn <arizkirizaldi@email.unikom.ac.id>
2021-02-18 13:50:38 +0700

    Stable Better

--- app/Http/Controllers/Pengiriman.php
+++ app/Http/Controllers/Pengiriman.php
@@ -50,17 +50,19 @@ class Pengiriman extends Controller
                 }
             }
         }
-        \Debugbar::debug($penghematan);
         //Iterasi
         $iterasi = 0;
         $check = [];
         $rute_all = [];
+        $temp = 0;
+        $rute = [];
         while (true){
-            $rute = [];
+            \Debugbar::debug($penghematan);
             $iterasi++;
             $max_penghematan = -1;
             $max_kolom = -1;
             $max_baris = -1;
+            \Debugbar::debug('Iterasi ke = '.$iterasi);
             foreach (array_keys($penghematan) as $index_kolom){
                 foreach (array_keys($penghematan[$index_kolom]) as $index_baris){
                     if($max_penghematan<$penghematan[$index_kolom][$index_baris]){
@@ -71,36 +73,80 @@ class Pengiriman extends Controller
                 }
             }
             if($max_penghematan ==0){
+                    array_push($rute_all,$rute);
+                \Debugbar::debug('Rute Final Terbentuk = '.print_r($rute_all,1));
+                \Debugbar::debug('Iterasi Selesai Dengan Semua Rute = '.print_r($rute_all,1));
+
                 break;
             }
-//            if($iterasi == 1){
-                $jumlah = $pemesanans[$max_kolom-1]->jumlah + $pemesanans[$max_baris-1]->jumlah;
-                if($jumlah < $kendaraan->kapasitas){
-                    array_push($rute,$max_kolom);
-                    array_push($rute,$max_baris);
-                    array_push($check,$max_baris);
-                    array_push($check,$max_kolom);
-                    array_push($rute_all,$rute);
-                }
-                else{
+            if($iterasi == 1) {
+                $jumlah = $pemesanans[$max_kolom - 1]->jumlah + $pemesanans[$max_baris - 1]->jumlah;
+                if ($jumlah < $kendaraan->kapasitas) {
+                    array_push($rute, $max_kolom);
+                    array_push($rute, $max_baris);
+                    array_push($check, $max_baris);
+                    array_push($check, $max_kolom);
+//                    array_push($rute_all,$rute)
+                    $temp = $temp + $jumlah;
+                    \Debugbar::debug('Rute Terbentuk = '.print_r($rute,1));
+                } else {
                     //jadi 2 rute
-                    if(!in_array($max_kolom,$check)){
-                        array_push($rute,$max_kolom);
-                        array_push($check,$max_kolom);
-                        array_push($rute_all,$rute);
-                    }
-                    $rute = [];
-                    if(!in_array($max_baris,$check)) {
-                        array_push($rute, $max_baris);
-                        array_push($check, $max_baris);
+                    if (!in_array($max_kolom, $check)) {
+                        array_push($rute, $max_kolom);
+                        array_push($check, $max_kolom);
                         array_push($rute_all, $rute);
                     }
+                    $rute = [];
+                    if (!in_array($max_baris, $check)) {
+                        array_push($rute, $max_baris);
+                        array_push($check, $max_baris);
+//                        array_push($rute_all, $rute);
+                        $temp = $temp + $jumlah;
+                    }
                 }
+            }
+            else{
+                \Debugbar::debug('Temp Sekarang = '.print_r($temp,1));
+                $jumlah = $temp + $pemesanans[$max_kolom-1]->jumlah;
+                if($jumlah<$kendaraan->kapasitas){
+                    array_push($rute,$max_kolom);
+                    $temp = $temp + $jumlah;
+                    \Debugbar::debug('Rute Terbentuk = '.print_r($rute,1));
+                }
+                else{
+                    array_push($rute_all,$rute);
+                    \Debugbar::debug('Rute Final Terbentuk = '.print_r($rute_all,1));
+                    $temp = 0;
+                    $rute = [];
+                    \Debugbar::debug('Temp Sekarang = '.print_r($temp,1));
+                    array_push($rute,$max_kolom);
+                    $temp = $temp + $pemesanans[$max_kolom-1]->jumlah;
+                    \Debugbar::debug('Rute Terbentuk = '.print_r($rute,1));
+                }
+                \Debugbar::debug('Temp Sekarang = '.print_r($temp,1));
+                $jumlah = $temp + $pemesanans[$max_baris-1]->jumlah;
+                if($jumlah<$kendaraan->kapasitas){
+                    array_push($rute,$max_baris);
+                    $temp = $temp + $jumlah;
+                    \Debugbar::debug('Rute Terbentuk = '.print_r($rute,1));
+                }
+                else{
+                    array_push($rute_all,$rute);
+                    \Debugbar::debug('Rute Final Terbentuk = '.print_r($rute_all,1));
+                    $temp = 0;
+                    $rute = [];
+                    array_push($rute,$max_baris);
+                    $temp = $temp + $pemesanans[$max_baris-1]->jumlah;
+                    \Debugbar::debug('Rute Terbentuk = '.print_r($rute,1));
+
+                }
+            }
+
 
             //Hapus setelah di cari penghematan
             foreach (array_keys($penghematan) as $index_kolom){
                 foreach (array_keys($penghematan[$index_kolom]) as $index_baris){
-                    \Debugbar::debug('Sekarang di '.$index_kolom.$index_baris);
+//                    \Debugbar::debug('Sekarang di '.$index_kolom.$index_baris);
                     if($index_baris == $max_baris OR $index_kolom == $max_kolom OR $max_baris == $index_kolom OR $max_kolom == $index_baris){
                         $penghematan[$index_kolom][$index_baris] = 0;
                         \Debugbar::debug($index_kolom.' '.$index_baris.'= 0');
