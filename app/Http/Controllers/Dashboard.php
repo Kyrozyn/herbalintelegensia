6f24f606bdeb9e46c5c4fdc0546f6f90e6c148a2
kyrozyn <arizkirizaldi@email.unikom.ac.id>
2021-02-18 23:06:41 +0700

    done

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -6,12 +6,15 @@ namespace App\Http\Controllers;
 
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class Dashboard extends Controller
 {
     public function tes()
     {
-        request()->user()->hasRole('admin');
-        return view('Dashboard.index',['title' => 'Dashboard']);
+        $pemesanans = DB::Select("Select count(DATE (`tanggal_pemesanan`)) as `jumlah`, DATE(`tanggal_pemesanan`) as `tanggal_pemesanan` from pemesanans group by DATE(`tanggal_pemesanan`) ORDER BY DATE(`pemesanans`.`tanggal_pemesanan`) DESC LIMIT 10");
+        $pemesanans_belum_dikirim = DB::Select("Select count(DATE (`tanggal_pemesanan`)) as `jumlah`, DATE(`tanggal_pemesanan`) as `tanggal_pemesanan` from pemesanans where `status` = 'Belum Dikirim' group by DATE(`tanggal_pemesanan`) ORDER BY DATE(`pemesanans`.`tanggal_pemesanan`) DESC LIMIT 10 ");
+        $pemesanans_dikirim = DB::Select("Select count(DATE (`tanggal_pemesanan`)) as `jumlah`, DATE(`tanggal_pemesanan`) as `tanggal_pemesanan` from pemesanans where `status` = 'Dikirim' group by DATE(`tanggal_pemesanan`) ORDER BY DATE(`pemesanans`.`tanggal_pemesanan`) DESC LIMIT 10 ");
+        return view('Dashboard.index',['title' => 'Beranda','pemesanans'=>$pemesanans,'pemesanans_belum_dikirim' => $pemesanans_belum_dikirim,'pemesanans_dikirim' => $pemesanans_dikirim]);
     }
 }
